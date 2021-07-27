7a0e16e1f915c9e5c7ff7aa68cf59aee0f3429cb
DanieleS5 <83968716+DanieleS5@users.noreply.github.com>
2021-07-27 12:05:44 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,181 @@
+$(function(){
+
+    var currentValue = 0;
+    var IsDrag = false;
+    var precoMaximo = 70000;
+    var precoAtual = 0;
+
+    $('.pointer-barra').mousedown(function(){
+        isDrag = true;
+
+    })
+
+    $(document).mouseup(function(){
+        isDrag - false;
+    })
+
+    $('.barra-preco').mousemove(function(e){
+        if(isDrag){
+            var elBase = $(this);
+            var mouseX = e.pageX - elBase.offset().left;
+            if(mouseX < 0)
+                mouseX = 0;
+                if(mouseX > elBase.width())
+                    mouseX = elBase.width();
+
+                $('.pointer-barra').css('left',(mouseX-13)+'px');
+                var currentValue = (mouseX / elBase.width()) * 100;
+                $('.barra-preco-fill').css('width',currentValue+ '%');
+            
+                //TODO: AJUSTAR O FORMATO DO PREÇO
+                precoAtual = (currentValue / 100) *precoMaximo;
+                precoAtual = formatarpreco(precoAtual);
+                
+                $('.precoPesquisa').html('R$'+precoAtual);
+       
+            }
+
+            function formatarpreco(precoAtual){
+                precoAtual = precoAtual.toFixed(2);
+                preco_arr = precoAtual.split('.');
+                
+                var novoPreco = formatarTotal(preco_arr);
+
+                return novoPreco;
+            }
+
+            function formatarTotal(preco_arr){
+                // if(preco_arr[1] == '00'){
+                //     return preco_arr[0]+','+preco_arr[1];
+
+                // } else{
+
+                    if(preco_arr[0] < 1000){
+                        return preco_arr[0]+','+preco_arr[1];
+                    } else if(preco_arr[0] < 10000){
+                        return preco_arr[0][0]+'.'+preco_arr[0].substr(1,preco_arr[0].length)+
+                        ','+preco_arr[1];
+                    } else{
+                        return preco_arr[0][0]+preco_arr[0][1]+'.'+preco_arr[0].substr(1,preco_arr[0].length)+
+                        ','+preco_arr[1];
+                    }
+                
+            }
+
+    function disableTextSelection(){
+        $("body").css("-webkit-user-select","auto");
+        $("body").css("-moz-user-select","auto");
+        $("body").css("-ms-user-select","auto");
+        $("body").css("-o-user-select","auto");
+        $("body").css("user-select","auto");
+
+    }
+
+    function enableTextSelection(){
+        $("body").css("-webkit-user-select","none");
+        $("body").css("-moz-user-select","none");
+        $("body").css("-ms-user-select","none");
+        $("body").css("-o-user-select","none");
+        $("body").css("--user-select","none");
+    }
+    })
+
+    // SISTEMA DE SLIDER PAGINA VEICUL 1
+
+
+    // foto destaque => style="background-image: url('image/carro1.jpg');"
+    //  border: 6px solid rgb(138, 137, 137);"
+
+    var imgShow = 3;
+    var maxIndex = Math.ceil($('.mini-img-wraper').length/3) - 1;
+    var curIndex = 0;
+
+    initSlider();
+    navegateSlider();
+    clickSlider();
+
+    function initSlider(){
+        var amt = $('.mini-img-wraper').length * 33.3;
+        var elScroll = $('.nav-galeria-wraper');
+        var enSingle = $('.mini-img-wraper');
+        elScroll.css('width',amt+'%');
+        enSingle.css('width',33.3*(100/amt)+'%');
+
+
+    }
+
+    function navegateSlider(){
+        $('.arrow-right-nav').click(function(){
+            if(curIndex < maxIndex){
+                curIndex++;
+                var elOff = $('.mini-img-wraper').eq(curIndex*3).offset().left - $('.nav-galeria-wraper').offset().left;
+                $('.nav-galeria').animate({'scrollLeft':elOff+'px'});
+            }else {
+                // console.log("Chegamos");
+            }
+        });
+
+        $('.arrow-left-nav').click(function(){
+            if(curIndex > 0){
+                curIndex--;
+                var elOff = $('.mini-img-wraper').eq(curIndex*3).offset().left - $('.nav-galeria-wraper').offset().left;
+                $('.nav-galeria').animate({'scrollLeft':elOff+'px'});
+            }else {
+                // console.log("Chegamos ao inicio");
+            }
+        });
+        
+    }
+
+    function clickSlider(){
+        $('.mini-img-wraper').click(function(){
+            $('.mini-img-wraper').css('background-color','transparent');
+            $(this).css('background-color','rgb(120, 120, 120)');
+            var img = $(this).children().css('background-image');
+            $('.foto-destaque').css('background-image',img);
+        })
+
+        $('.mini-img-wraper').eq(0).click();
+    }  
+
+    // CLICAR E IR PARA DIV DE CONTATO COM BASE NO ATRIBUTO GOTO
+
+    // $('[goto=contato]').click(function(){
+
+    //     $('nav a').css('color', 'black');
+    //     $(this).css('color','#EB2D2D');
+    //     $('html,body').animate({'scrollTop':$('#contato').offset().top});
+
+    //     return false;
+    // });
+
+    var directory = '/Projetos_Web/Projeto_006/'
+
+    $('[goto=contato]').click(function(){
+
+        location.href=directory+'index.html?contato';
+        return false;
+    })
+
+    checkUrl();
+
+    function checkUrl(){
+        var url = location.href.split('/');
+        var curPage = url[url.length-1].split('?');
+
+        if(curPage[1] != undefined && curPage[1] == 'contato'){
+            $('header nav a').css('color','black');
+            $('footer nav a').css('color','white');
+            $('[goto=contato]').css('color','#EB2D2D');
+            $('html, body').animate({'scrollTop':$('#contato').offset().top});
+        }
+    
+    }
+
+    // menu responsivo
+
+    $('.mobile').click(function(){
+        $(this).find('ul').sliderToggle();
+    })
+
+ });
